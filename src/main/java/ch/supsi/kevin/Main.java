8c0d18ab402dcaa0678eeecddd12fa00f5790541
Scrotuss <scroto@protonmail.ch>
2020-03-05 08:44:40 +0100

    add .keep files for having same folder structure on bot laptops

--- src/main/java/ch/supsi/kevin/Main.java
+++ src/main/java/ch/supsi/kevin/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args)  {
-        System.out.println("hello, world");
+        System.out.println("hello, world banana");
         File f = new File("src/main/resources/ch130.tsp");
         try {
             TspFileToData.convert(f);
