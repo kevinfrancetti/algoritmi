25960b5a7b1d878da4b4245ca1a8626a6f113e4a
kevinfrancetti <kevin.francetti91@gmail.com>
2020-05-15 20:42:42 +0200

    seems like multifragment is working

--- src/main/java/ch/supsi/kevin/algos/constructive/MultiFragment.java
+++ src/main/java/ch/supsi/kevin/algos/constructive/MultiFragment.java
@@ -1,10 +1,7 @@
 package ch.supsi.kevin.algos.constructive;
 
 import ch.supsi.kevin.Main;
-import ch.supsi.kevin.datastructure.Edge;
-import ch.supsi.kevin.datastructure.ListType;
-import ch.supsi.kevin.datastructure.Point;
-import ch.supsi.kevin.datastructure.TspData;
+import ch.supsi.kevin.datastructure.*;
 
 import java.util.*;
 import java.util.List;
@@ -12,95 +9,77 @@ import java.util.List;
 public class MultiFragment {
 
 
-    public static void solve(TspData tspData){
+    public static void solve(TspData tspData) {
         List<Point> inputPoints = tspData.toListOfPoint(ListType.LINKED);
         List<Point> outputPoints = new LinkedList<>();
         List<Edge> edges = new LinkedList<>();
         List<Edge> outputArchList = new LinkedList<>();
 
         /*Getting all the possible edges*/
-        for(int i = 0; i < inputPoints.size(); i++){
-            for(int j = i+1; j < inputPoints.size(); j++){
+        for (int i = 0; i < inputPoints.size(); i++) {
+            for (int j = i + 1; j < inputPoints.size(); j++) {
                 edges.add(new Edge(inputPoints.get(i), inputPoints.get(j)));
                 System.out.println("i: " + i + " j: " + j);
             }
         }
 
-        for(Edge a : edges){
+        for (Edge a : edges) {
             System.out.println(a.distance);
         }
         System.out.println("==========");
 
         /*Sorting in ascending order*/
-        edges.sort((Edge a1, Edge a2)->{
+        edges.sort((Edge a1, Edge a2) -> {
             float delta = a1.distance - a2.distance;
-            if(delta > 0) return 1;
-            if(delta < 0) return -1;
+            if (delta > 0) return 1;
+            if (delta < 0) return -1;
             return 0;
-        } );
+        });
 
-        for(Edge a : edges){
+        for (Edge a : edges) {
             System.out.println(a.distance);
         }
         System.out.println(edges.size());
 
-        /*Describe how many connection has a point*/
-        Map<Point, Integer> usedPoints = new HashMap<>();
+        for (Edge edge : edges) {
 
-        for(Edge edge : edges){
-
-            if(!usedPoints.containsKey(edge.p1)){
-                usedPoints.put(edge.p1, 0);
+            /*Check conditions*/
+            if(edge.p1.neighbours.size() >= 2 || edge.p2.neighbours.size() >=2) continue;
+            Point.connect(edge.p1, edge.p2);
+            if(checkIfCyclic(edge)){
+                Point.disconnect(edge.p1, edge.p2);
+                continue;
             }
-            if(!usedPoints.containsKey(edge.p2)){
-                usedPoints.put(edge.p2, 0);
-            }
-
-            if(usedPoints.get(edge.p1) >= 1 && usedPoints.get(edge.p2) >= 1) continue;
-            if(usedPoints.get(edge.p1) >= 2 || usedPoints.get(edge.p2) >= 2) continue;
-
-            usedPoints.put(edge.p1, usedPoints.get(edge.p1) + 1);
-            usedPoints.put(edge.p2, usedPoints.get(edge.p2) + 1);
             outputArchList.add(edge);
 
         }
 
 
-        for(Edge arch : outputArchList){
+        for (Edge arch : outputArchList) {
             System.out.println(arch);
         }
         System.out.println(outputArchList.size());
     }
 
-    private static boolean checkAndUpdate(Map<Point, Integer> memory, Point point){
-        if(memory.containsKey(point)){
-            int tmpPointValue = memory.get(point);
-            if(tmpPointValue >= 2) return false;
-            else memory.put(point, tmpPointValue+1);
-        }else {
-            memory.put(point, 1);
-            return true;
+    static boolean recursiveCheck(Point p, Point previous, Set<Point> memory) {
+        memory.add(p);
+        for (Point neighbour : p.neighbours) {
+            if (neighbour.equals(previous)) continue;
+            if (memory.contains(neighbour)) return true;
+            if (recursiveCheck(neighbour, p, memory)) return true;
         }
-        return true;
+        return false;
     }
 
-    private static boolean checkAndUpdate2(Map<Point, Integer> memory, Point point1, Point point2){
-        if(memory.containsKey(point1) && memory.containsKey(point2)) return false;
-        if(memory.containsKey(point1)){
-            int tmpPointValue = memory.get(point1);
-            if(tmpPointValue >= 2) return false;
-            else memory.put(point1, tmpPointValue+1);
-        }else {
-            memory.put(point1, 1);
-            return true;
-        }
-        return true;
+    static boolean checkIfCyclic(Edge e) {
+        Set<Point> memory = new HashSet<>();
+        return recursiveCheck(e.p1, e.p1, memory) && recursiveCheck(e.p2, e.p2, memory);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Map<String, TspData> map = TspData.folderToMapOfTspData(Main.FOLDER_PATH);
-        TspData data = map.get("fake.tsp");
+        TspData data = map.get("pcb442.tsp");
         solve(data);
 
         System.out.println("Yolo");
--- src/main/java/ch/supsi/kevin/tmp/CycleTest.java
+++ src/main/java/ch/supsi/kevin/tmp/CycleTest.java
@@ -2,21 +2,21 @@ package ch.supsi.kevin.tmp;
 
 import java.util.*;
 
-class LP {
+class LinkedPoint {
     int x, y;
     int name;
-    List<LP> neighbours = new LinkedList<>();
-    public LP(int x, int y){
+    List<LinkedPoint> neighbours = new LinkedList<>();
+    public LinkedPoint(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    public LP(int name){
+    public LinkedPoint(int name){
         this(0,0);
         this.name = name;
     }
 
-    boolean add(LP point){
+    boolean add(LinkedPoint point){
         //if(neighbours.size() >= 2) return false;
         //if(point.neighbours.size() >=2) return false;
         neighbours.add(point);
@@ -33,9 +33,9 @@ class LP {
 
 public class CycleTest {
 
-    static boolean isCycle(LP p, LP previous, Set<LP> memory){
+    static boolean isCycle(LinkedPoint p, LinkedPoint previous, Set<LinkedPoint> memory){
         memory.add(p);
-        for(LP neighbour : p.neighbours){
+        for(LinkedPoint neighbour : p.neighbours){
             if(neighbour.equals(previous)) continue;
             if(memory.contains(neighbour)) return true;
             if(isCycle(neighbour, p, memory)) return true;
@@ -43,19 +43,19 @@ public class CycleTest {
         return false;
     }
 
-    static void starter(LP p){
-        Set<LP> memory = new HashSet<>();
+    static void starter(LinkedPoint p){
+        Set<LinkedPoint> memory = new HashSet<>();
         System.out.println(isCycle(p, p, memory));
     }
 
     public static void main(String[] args){
         System.out.println("Yolo");
 
-        LP p1 = new LP(1);
-        LP p2 = new LP(2);
-        LP p3 = new LP(3);
-        LP p4 = new LP(4);
-        LP p5 = new LP(5);
+        LinkedPoint p1 = new LinkedPoint(1);
+        LinkedPoint p2 = new LinkedPoint(2);
+        LinkedPoint p3 = new LinkedPoint(3);
+        LinkedPoint p4 = new LinkedPoint(4);
+        LinkedPoint p5 = new LinkedPoint(5);
 
         p1.add(p2);
         p2.add(p3);
